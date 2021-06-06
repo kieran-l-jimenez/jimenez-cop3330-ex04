c809c2effc6673461713a6108b5674b4b657c8f0
Kieran Jimenez <kieran.l.jimenez@gmail.com>
2021-06-06 13:40:13 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Kieran Jimenez
+ */
+/*
+*           "Madlib" output
+*    Enter a noun: dog
+*    Enter a verb: walk
+*    Enter an adjective: blue
+*    Enter an adverb: quickly
+*    Do you walk your blue dog quickly? That's hilarious!
+ */
+public class App
+{
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        App myApp = new App();
+
+        String noun = myApp.wordPrompt("noun");
+        String verb = myApp.wordPrompt("verb");
+        String adjective = myApp.wordPrompt("adjective");
+        String adverb = myApp.wordPrompt("adverb");
+
+        System.out.println(String.format("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb));
+    }
+
+    public String wordPrompt(String wordType)
+    {
+        System.out.print("Enter a " + wordType + ": ");
+        return in.nextLine();
+    }
+
+}
